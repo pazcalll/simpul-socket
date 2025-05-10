8f8653f0fa57941bfff0e043ba99679453354f39
pazcalll <yazeedarifin@gmail.com>
2025-05-11 05:03:46 +0700

    feat: layout update + break words

--- app/javascript/components/Balloon.tsx
+++ app/javascript/components/Balloon.tsx
@@ -2,7 +2,7 @@ export default function Balloon({ sender, message, formattedDate, isRTL=false }:
   return (
     <div className={`flex flex-col ${isRTL ? 'items-start' : 'items-end'} my-6`}>
       <span className="text-sm font-bold">{sender}</span>
-      <div className={`${isRTL ? 'bg-gray-300' : 'bg-green-200'} rounded-lg p-2 mt-1 max-w-[80%]`}>
+      <div className={`${isRTL ? 'bg-gray-300' : 'bg-green-200'} rounded-lg p-2 mt-1 max-w-[80%] break-words`}>
         <p>{message}</p>
       </div>
       <span className="text-sm text-gray-300">{formattedDate}</span>
--- app/javascript/components/Layout.tsx
+++ app/javascript/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Head } from "@inertiajs/react"
 export default function Layout({ children }: { children: React.ReactNode }) {
     return <>
         <Head title="Group Chat App" />
-        <div className={`absolute max-h-screen overflow-y-hidden overflow-x-hidden max-w-full w-full md:w-[44rem] md:max-w-[44rem]`}>
+        <div className={`top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute max-h-screen overflow-y-hidden overflow-x-hidden max-w-full w-full md:w-[44rem] md:max-w-[44rem] bg-white`}>
         {/* <div className={`${cs.root} max-h-screen overflow-y-hidden overflow-x-hidden max-w-[30rem] w-[30rem] md:w-[44rem] md:max-w-[44rem]`}> */}
             {children}
         </div>
